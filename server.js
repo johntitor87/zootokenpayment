3c473c4077c6cf91ff7c34686a72ed356db82a36
Mark Hendrickson <markhendrixks987@gmail.com>
2026-02-21 22:59:00 -0800

    Fix: remove dist reference for Render deploy

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-require('./dist/index');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('ZOO Solana Checkout API Running');
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
